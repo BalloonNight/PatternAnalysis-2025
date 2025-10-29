9a025eee5a65adaf557658421bf4665eda303f1d
BalloonNight <baileyrnshw@gmail.com>
2025-10-29 10:32:26 +1000

    improved module coments and structure
    added a much of documentation to modules.py, improved variable naming, changed the structure of the code a little, for example including the conv always before the context module into the context module, as well as class/function inputs

--- recognition/3D Improved UNet 48025582/modules.py
+++ recognition/3D Improved UNet 48025582/modules.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-model.py
+"""model.py
 An implementation of the 3D Improved UNet3D module from [2].
 
 References for [2] can be found in README.md
@@ -8,172 +7,316 @@ References for [2] can be found in README.md
 import torch
 import torch.nn as nn
 
+# global constants
 DEFAULT_DROPOUT = 0.3
 NEGATIVE_SLOPE = 1e-2
 N_CLASSES = 6
 BASE_FILTERS = 16
 
 class ContextModule(nn.Module):
-    def __init__(self, in_ch, out_ch, dropout_p=DEFAULT_DROPOUT):
-        super().__init__()
-        self.norm1 = nn.InstanceNorm3d(in_ch)
-        self.relu1 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
-        self.conv1 = nn.Conv3d(in_ch, out_ch, kernel_size=3, stride=1,
-                               padding=1, bias=False)
-        self.drop1 = nn.Dropout3d(p=dropout_p)
-        self.norm2 = nn.InstanceNorm3d(out_ch)
-        self.relu2 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
-        self.conv2 = nn.Conv3d(out_ch, out_ch, kernel_size=3, stride=1,
-                               padding=1, bias=False)
+    """This class handles the context module for the UNet algorithm.
 
-    def forward(self, x):
-        out = self.norm1(x)
+    as per [2], this is a pre-activation residual block with 2 3x3x3 convolution
+    layers and a dropout layer in between and a skip connection from the input
+    to output. does include the conv always before the context model too and
+    implements the skip afterwards like normal.
+
+    Attributes:
+        convolution1: pre conv layer
+        normalize1: 1st layer InstanceNorm3d.
+        relu1: 1st layer LeakyReLU.
+        convolution2: 1st layer Conv3d.
+        dropout: Dropout3d layer.
+        normalize2: 2nd layer InstanceNorm3d.
+        relu2: 2nd layer LeakyReLU.
+        convolution3: 2nd layer Conv3d.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int = None, stride:
+    int = 2, dropout_p: float =
+    DEFAULT_DROPOUT):
+        """Initializes the Context Module.
+
+        Args:
+            in_channels (int): num of in channels.
+            out_channels (int): num of out channels, default double in channels.
+            stride (int): stride, default 2.
+            dropout_p (float): probability of each channel being randomly zeroed
+             out, default DEFAULT_DROPOUT.
+        """
+        super().__init__()
+        if out_channels is None:
+            out_channels = in_channels * 2
+        # pre conv
+        self.convolution1 = nn.Conv3d(in_channels, out_channels, kernel_size=3,
+                                      stride=stride, padding=1, bias=False)
+        # layer 1
+        self.normalize1 = nn.InstanceNorm3d(out_channels)
+        self.relu1 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
+        self.convolution2 = nn.Conv3d(out_channels, out_channels, kernel_size=3,
+                                      stride=1, padding=1, bias=False)
+        # connection
+        self.dropout = nn.Dropout3d(p=dropout_p)
+        # layer 2
+        self.normalize2 = nn.InstanceNorm3d(out_channels)
+        self.relu2 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
+        self.convolution3 = nn.Conv3d(out_channels, out_channels, kernel_size=3,
+                                      stride=1, padding=1, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Performs a forward pass through the context module.
+
+        Args:
+            x (torch.Tensor): The input feature map.
+
+        Returns:
+            torch.Tensor: the forward passed feature map through the context
+             module.
+        """
+        # pre conv
+        out = self.convolution1(x)
+        residual = out
+        # layer 1
+        out = self.normalize1(out)
         out = self.relu1(out)
-        out = self.conv1(out)
-        out = self.drop1(out)
-        out = self.norm2(out)
+        out = self.convolution2(out)
+        # connection
+        out = self.dropout(out)
+        # layer 2
+        out = self.normalize2(out)
         out = self.relu2(out)
-        out = self.conv2(out)
-        return out
+        out = self.convolution3(out)
+        # add skip connection
+        return out + residual
 
 
 class LocalizationModule(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super().__init__()
-        self.norm1 = nn.InstanceNorm3d(in_ch)
-        self.relu1 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
-        self.conv1 = nn.Conv3d(in_ch, out_ch, kernel_size=3, stride=1,
-                               padding=1, bias=False)
-        self.norm2 = nn.InstanceNorm3d(out_ch)
-        self.relu2 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
-        self.conv2 = nn.Conv3d(out_ch, out_ch, kernel_size=1, stride=1,
-                               padding=0, bias=False)
+    """This class handles the localization module for the algorithm.
 
-    def forward(self, x):
-        out = self.norm1(x)
+    as per [2], this consists of a 3x3x3 convolution followed by a 1x1x1
+    convolution which halves the number of feature maps.
+
+    Attributes:
+        normalization1: 1st layer InstanceNorm3d.
+        relu1: 1st layer LeakyReLU.
+        convolution1: 1st layer Conv3d.
+        normalization2: 2nd layer InstanceNorm3d.
+        relu2: 2nd layer LeakyReLU.
+        convolution2: 2nd layer Conv3d.
+    """
+
+    def __init__(self, in_channels: int):
+        """Initializes the Localization Module.
+
+        Args:
+            in_channels: number of input channels
+        """
+        super().__init__()
+        out_channels = in_channels // 2
+        # layer 1
+        self.normalization1 = nn.InstanceNorm3d(in_channels)
+        self.relu1 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
+        self.convolution1 = nn.Conv3d(in_channels, in_channels, kernel_size=3,
+                                      stride=1, padding=1, bias=False)
+        # layer 2
+        self.normalization2 = nn.InstanceNorm3d(in_channels)
+        self.relu2 = nn.LeakyReLU(negative_slope=NEGATIVE_SLOPE)
+        self.convolution2 = nn.Conv3d(in_channels, out_channels, kernel_size=1,
+                                      stride=1, padding=0, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        """Performs a forward pass through the localization module.
+
+        Args:
+            x (torch.Tensor): The input feature map.
+
+        Returns:
+            torch.Tensor: the forward passed feature map through the
+             localization module.
+        """
+        # layer 1
+        out = self.normalization1(x)
         out = self.relu1(out)
-        out = self.conv1(out)
-        out = self.norm2(out)
+        out = self.convolution1(out)
+        # layer 2
+        out = self.normalization2(out)
         out = self.relu2(out)
-        out = self.conv2(out)
+        out = self.convolution2(out)
         return out
 
 
 class UpsamplingModule(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super().__init__()
-        self.upsp1 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.conv1 = nn.Conv3d(in_ch, out_ch, kernel_size=3, stride=1,
-                               padding=1, bias=False)
+    """This class handles the upsampling module for the algorithm.
 
-    def forward(self, x):
-        out = self.upsp1(x)
-        out = self.conv1(out)
+    As per [2], this consists an upscale that repeats feature voxels twice, and
+    then a 3x3x3 convolution that half's the feature map.
+
+    Attributes:
+        upsample: A Upsample layer.
+        convolution: A Conv3d layer.
+    """
+
+    def __init__(self, in_channels):
+        """Initializes the Upsampling Module.
+
+        Args:
+            in_channels: number of input channels
+        """
+        super().__init__()
+        out_channels = in_channels // 2
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.convolution = nn.Conv3d(in_channels, out_channels, kernel_size=3,
+                                     stride=1, padding=1, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        """Performs a forward pass through the upsampling module.
+
+        Args:
+            x (torch.Tensor): The input feature map.
+
+        Returns:
+            torch.Tensor: the forward passed feature map through the
+             upsampling module.
+        """
+        out = self.upsample(x)
+        out = self.convolution(out)
         return out
 
-class ImprovedUNet3D2(nn.Module):
-    def __init__(self, in_ch: int, filters=BASE_FILTERS, n_classes=N_CLASSES,
-                 dropout_p=DEFAULT_DROPOUT):
+class ImprovedUNet3D(nn.Module):
+    """This class handles the 3D Improved UNet3D model.
+
+    as per [2], this consists of a 5 layer context pathway and 5 layer
+    localization pathway, with residual connections between the pathways, and
+    the last 3 layers of the localization pathway being combined for the final
+    output. The number in the names of the modules in attributes represents
+    their depth in the model, which is determined by the verticality of the
+    module in diagram, figure [INSERT FIGURE NUMBER], in README.md.
+
+    Attributes:
+        in_channels: number of starting channels.
+        filters: number of filters.
+        n_classes: number of classes.
+        dropout_p: chance of channels being set to 0 during training.
+        context1: 1st layer context module.
+        context2: 2nd layer context module.
+        context3: 3rd layer context module.
+        context4: 4th layer context module.
+        context5: 5th layer context module.
+        upsample5: 5th layer upsample module.
+        localization4: 4th layer localization module.
+        upsample4: 4th layer upsample module.
+        localization3: 3rd layer localization module.
+        upsample3: 3rd layer upsample module.
+        localization2: 2nd layer localization module.
+        upsample2: 2nd layer upsample module.
+        convolution1: 1st layer localization module.
+        segment3: 3rd layer segmentation layer.
+        upscale3: 3rd layer upscale.
+        segment2: 2nd layer segmentation layer.
+        upscale2: 2nd layer upscale.
+        segment1: 1st layer segmentation layer.
+        smax1: 1st layer softmax.
+    """
+    def __init__(self, in_channels: int, filters=BASE_FILTERS,
+                 n_classes=N_CLASSES, dropout_p=DEFAULT_DROPOUT):
+        """Initializes the 3D Improved UNet 3D module.
+
+        Args:
+            in_channels: number of starting channels.
+            filters: number of filters.
+            n_classes: number of classes.
+            dropout_p: chance of channels being set to 0 during training.
+        """
         super().__init__()
-        self.in_ch = in_ch
+        # input vars
+        self.in_channels = in_channels
         self.filters = filters
         self.n_classes = n_classes
         self.dropout_p = dropout_p
 
-        # Encoder
-        self.conv1 = nn.Conv3d(self.in_ch, self.filters, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.cont1 = ContextModule(self.filters, self.filters, self.dropout_p)
-        self.conv2 = nn.Conv3d(self.filters, self.filters * 2, kernel_size=3,
-                               stride=2, padding=1, bias=False)
-        self.cont2 = ContextModule(self.filters * 2, self.filters * 2,
-                                   self.dropout_p)
-        self.conv3 = nn.Conv3d(self.filters * 2, self.filters * 4,
-                               kernel_size=3, stride=2, padding=1, bias=False)
-        self.cont3 = ContextModule(self.filters * 4, self.filters * 4,
-                                   self.dropout_p)
-        self.conv4 = nn.Conv3d(self.filters * 4, self.filters * 8,
-                               kernel_size=3, stride=2, padding=1, bias=False)
-        self.cont4 = ContextModule(self.filters * 8, self.filters * 8,
-                                   self.dropout_p)
-        self.conv5 = nn.Conv3d(self.filters * 8, self.filters * 16,
-                               kernel_size=3, stride=2, padding=1, bias=False)
-        self.cont5 = ContextModule(self.filters * 16, self.filters * 16,
-                                   self.dropout_p)
+        # Encoder/Context Pathway
+        self.context1 = ContextModule(self.in_channels, self.filters, 1,
+                                      self.dropout_p)
+        self.context2 = ContextModule(self.filters, dropout_p=self.dropout_p)
+        self.context3 = ContextModule(self.filters * 2,
+                                      dropout_p=self.dropout_p)
+        self.context4 = ContextModule(self.filters * 4,
+                                      dropout_p=self.dropout_p)
+        self.context5 = ContextModule(self.filters * 8,
+                                      dropout_p=self.dropout_p)
 
-        # Decoder
-        self.upsp1 = UpsamplingModule(self.filters * 16, self.filters * 8)
-        self.loca1 = LocalizationModule(self.filters * 8, self.filters * 8)
-        self.upsp2 = UpsamplingModule(self.filters * 8, self.filters * 4)
-        self.loca2 = LocalizationModule(self.filters * 4, self.filters * 4)
-        self.upsp3 = UpsamplingModule(self.filters * 4, self.filters * 2)
-        self.loca3 = LocalizationModule(self.filters * 2, self.filters * 2)
-        self.upsp4 = UpsamplingModule(self.filters * 2, self.filters)
-        self.conv6 = nn.Conv3d(self.filters, self.filters * 2, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        # Decoder/Localization Pathway
+        self.upsample5 = UpsamplingModule(self.filters * 16)
+        self.localization4 = LocalizationModule(self.filters * 16)
+        self.upsample4 = UpsamplingModule(self.filters * 8)
+        self.localization3 = LocalizationModule(self.filters * 8)
+        self.upsample3 = UpsamplingModule(self.filters * 4)
+        self.localization2 = LocalizationModule(self.filters * 4)
+        self.upsample2 = UpsamplingModule(self.filters * 2)
+        self.convolution1 = nn.Conv3d(
+            self.filters * 2, self.filters * 2,
+            kernel_size=3, stride=1, padding=1, bias=False)
 
         # Segmentation
-        self.segm1 = nn.Conv3d(self.filters * 4, self.n_classes, kernel_size=1,
-                               stride=1, padding=0, bias=False)
-        self.upsc1 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.segm2 = nn.Conv3d(self.filters * 2, self.n_classes, kernel_size=1,
-                               stride=1, padding=0, bias=False)
-        self.upsc2 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.segm3 = nn.Conv3d(self.filters, self.n_classes, kernel_size=1,
-                               stride=1, padding=0, bias=False)
-        self.smax = nn.Softmax(dim=1)
+        self.segment3 = nn.Conv3d(
+            self.filters * 4, self.n_classes,
+            kernel_size=1, stride=1, padding=0, bias=False)
+        self.upscale3 = nn.Upsample(scale_factor=2, mode='nearest')
+        self.segment2 = nn.Conv3d(
+            self.filters * 2, self.n_classes,
+            kernel_size=1, stride=1, padding=0, bias=False)
+        self.upscale2 = nn.Upsample(scale_factor=2, mode='nearest')
+        self.segment1 = nn.Conv3d(
+            self.filters * 2, self.n_classes,
+            kernel_size=1, stride=1, padding=0, bias=False)
+        self.smax1 = nn.Softmax(dim=1)
 
     def forward(self, x):
-        # Encoder
-        out = self.conv1(x)
-        res = out
-        out = self.cont1(out)
-        out += res
-        residual1 = out
-        out = self.conv2(out)
-        res = out
-        out = self.cont2(out)
-        out += res
-        residual2 = out
-        out = self.conv3(out)
-        res = out
-        out = self.cont3(out)
-        out += res
-        residual3 = out
-        out = self.conv4(out)
-        res = out
-        out = self.cont4(out)
-        out += res
-        residual4 = out
-        out = self.conv5(out)
-        res = out
-        out = self.cont5(out)
-        out += res
+        """Performs a forward pass through the 3D Improved UNet3D module.
 
-        # Decoder
-        out = self.upsp1(out)
-        out = torch.cat([out, residual4], dim=1)
-        out = self.loca1(out)
-        out = self.upsp2(out)
-        out = torch.cat([out, residual3], dim=1)
-        out = self.loca2(out)
-        to_seg1 = out
-        out = self.upsp3(out)
-        out = torch.cat([out, residual2], dim=1)
-        out = self.loca3(out)
-        to_seg2 = out
-        out = self.upsp4(out)
-        out = torch.cat([out, residual1], dim=1)
-        out = self.conv6(out)
+        Args:
+            x (torch.Tensor): The input feature map.
+
+        Returns:
+            torch.Tensor: the forward passed feature map through the
+             3D Improved UNet3D module.
+        """
+        # Encoder/Context Pathway
+        out = self.context1(x)
+        context_layer_1_out = out
+        out = self.context2(out)
+        context_layer_2_out = out
+        out = self.context3(out)
+        context_layer_3_out = out
+        out = self.context4(out)
+        context_layer_4_out = out
+        out = self.context5(out)
+
+        # Decoder/Localization Pathway
+        out = self.upsample5(out)
+        # note: concatenation doubles the number of output filters
+        out = torch.cat([out, context_layer_4_out], dim=1)
+        out = self.localization4(out)
+        out = self.upsample4(out)
+        out = torch.cat([out, context_layer_3_out], dim=1)
+        out = self.localization3(out)
+        localization_layer_3_out = out
+        out = self.upsample3(out)
+        out = torch.cat([out, context_layer_2_out], dim=1)
+        out = self.localization2(out)
+        localization_layer_2_out = out
+        out = self.upsample2(out)
+        out = torch.cat([out, context_layer_1_out], dim=1)
+        out = self.convolution1(out)
 
         # Segmentation
-        out_temp1 = self.segm1(to_seg1)
-        out_temp1 = self.upsc1(out_temp1)
-        out_temp2 = self.segm2(to_seg2)
-        out_temp2 += out_temp1
-        out_temp2 = self.upsc2(out_temp2)
-        out = self.segm3(out)
-        out += out_temp2
-        out = self.smax(out)
+        layer_3_segment = self.segment3(localization_layer_3_out)
+        layer_3_segment = self.upscale3(layer_3_segment)
+        layer_2_segment = self.segment2(localization_layer_2_out)
+        layer_2_segment += layer_3_segment
+        layer_2_segment = self.upscale2(layer_2_segment)
+        out = self.segment1(out)
+        out += layer_2_segment
+        out = self.smax1(out)
 
         return out
